6f16ff0434640a5e72470b5ed1e27fc5b5225586
gonzalo-asir17 <gonzalo.asir17@gmail.com>
2023-11-21 21:10:29 +0000

    Mi primer programa en python3: programa2.py

---
+++ programa2.py
@@ -0,0 +1,14 @@
+#Pseudocodigo
+# Definir edad como entero;
+# Escribir "Dime tu edad:";
+# Leer edad; 
+# Si edad >= 18 entonces
+#   Escribir "Eres mayor de edad";
+# FinSi
+# Escribir "Programa terminado";
+# FinPrcoeso
+
+edad = int(input("Dime tu edad: "))
+if edad >= 18:
+    print("Eres mayor de edad")
+print("Programa terminado")
