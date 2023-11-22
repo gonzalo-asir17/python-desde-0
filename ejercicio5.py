cef8184c3edde1e50199843a67602e62d916d9f9
gonzalo-asir17 <127349573+gonzalo-asir17@users.noreply.github.com>
2023-11-22 12:31:36 +0000

    Convertir ºC en Fahrenheit

---
+++ ejercicio5.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+#Escribir un programa que convierta un valor dado en grados Fahrenheit a grados Celsius. 
+#Recordar que la fórmula para la conversión es:
+#C = (F-32)*5/9
+
+import math
+
+fahrenheit = float(input("Ingrese la temperatura en grados Fahrenheit: "))
+celsius = round((fahrenheit-32)*5/9 , 3)
+print("La temperatura en grados Celsius es: ", celsius)
+
