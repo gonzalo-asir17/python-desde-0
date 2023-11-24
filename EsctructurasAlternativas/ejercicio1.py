6dbaeea4b965b78abe0cc0dbbbe232698e6d7cbb
gonzalo-asir17 <127349573+gonzalo-asir17@users.noreply.github.com>
2023-11-24 18:24:02 +0000

    Comprobar si un numero es mayor que otro

---
+++ EsctructurasAlternativas/ejercicio1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+#Hecho por Gonzalo Rodriguez Martin 24/11/2023
+# Algoritmo que pida dos números e indique si el primero es mayor que el segundo.
+
+numero1 = int(input("Introduzca un numero "))
+numero2 = int(input("Introduzca otro numero "))
+
+if numero1 > numero2:
+    print("El numero 1 es mayor que el numero 2")
+else:
+    print("El numero 2 es mayor que el numero 1")
+print("Programa terminado")
+
