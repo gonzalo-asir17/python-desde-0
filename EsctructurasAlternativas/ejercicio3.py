b60afda8659a2f26528ea50efc8c508a2166cc0f
gonzalo-asir17 <gonzalo.asir17@gmail.com>
2023-11-25 11:17:11 +0000

    Averiguar si un numero es par o impar

---
+++ EsctructurasAlternativas/ejercicio3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+#Hecho por Gonzalo Rodriguez Martin 24/11/2023
+#Escribe un programa que lea un número e indique si es par o impar.
+
+numero = int(input("Introduzca un numero "))
+
+if numero % 2 == 0:
+    print("El numero que introduces es par")
+else:
+    print("El numero es impar")
+print("Fin del programa")
