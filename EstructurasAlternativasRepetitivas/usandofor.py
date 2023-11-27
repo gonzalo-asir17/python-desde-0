d9f7bdd8e466e419212c8b807636f9b468c8aa70
gonzalo-asir17 <127349573+gonzalo-asir17@users.noreply.github.com>
2023-11-27 17:30:43 +0000

    Aprendiendo a usar for

---
+++ EstructurasAlternativasRepetitivas/usandofor.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+#Hecho por Gonzalo Rodriguez Martin 27/11/2023
+#Pseudocodigo 1
+#Proceso Contar 
+#Definir var como entero 
+#Para var <- 10 Hasta 10
+#Hacer
+#Escribir Sin Saltar var,"",:
+#Fin Proceso
+
+for var in range(1,11):
+    print(var," ", end = " ")
+print()
+print("Fin del Programa")
+
+#Pseudocodigo 2
+#Proceso de ContarDescendente 
+#Definir var como Entero 
+#Para ver de 10 a 1
+
+for var in range(10,0,-1):
+    print(var," ", end = " ")
+print()
+print("Fin del Programa")
+
+#Pseudocodigo 3
+#Proceso de contar pares
+for var in range(2,11,2):
+    print(var, " ", end = " ")
+print()
+print("Fin del Programa")
+
