abad3bb104eb4114d8954011ffede48c6add1e8e
gonzalo-asir17 <gonzalo.asir17@gmail.com>
2023-11-27 23:02:28 +0000

    Pedir un numero y hacer su factorial

---
+++ EstructurasAlternativasRepetitivas/ejercicio1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+#Hecho por Gonzalo Rodriguez Martin 27/11/2023
+
+#Crea una aplicación que pida un número y calcule su factorial (El factorial de un número 
+#es el producto de todos los enteros entre 1 y el propio número y se representa por el número 
+#seguido de un signo de exclamación. Por ejemplo 5! = 1x2x3x4x5=120),
+
+
+numero = int(input("Introduce un numero: "))
+factorial = 1
+for i in range(1, numero + 1):
+    factorial *= i
+print("El factorial de", numero, "es", factorial)
+print("Fin del programa")
+
