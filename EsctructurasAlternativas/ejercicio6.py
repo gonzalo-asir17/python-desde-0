ca185840d9dba13213dcb15bb31100d80e4247df
gonzalo-asir17 <gonzalo.asir17@gmail.com>
2023-11-27 01:02:28 +0000

    Comprobar si una letra mayuscula

---
+++ EsctructurasAlternativas/ejercicio6.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+#Hecho por Gonzalo Rodriguez Martin 27/11/2023
+# Programa que lea una cadena por teclado y compruebe si es una letra mayúscula.
+
+letra = input("Introduce una letra: ")
+
+if letra.isupper():
+    print("Es una letra mayuscula")
+else:
+    print("No es una letra mayuscula")
+print("Fin del programa")
+
